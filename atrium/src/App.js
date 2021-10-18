bac384ede52e648e618fb3de8a5a30b1cd039e2e
lucas-chave <lcchavesr150@gmail.com>
2021-10-18 15:40:09 -0300

    change function structure

--- atrium/src/App.js
+++ atrium/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
       <Route exact path="/" component={ Main } />
