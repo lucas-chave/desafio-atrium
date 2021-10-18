33d40b775552ae43bbbc9d4d000f6453eefa1ade
lucas-chave <lcchavesr150@gmail.com>
2021-10-18 15:50:43 -0300

    adding Headers component and importing it

--- atrium/src/App.js
+++ atrium/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Main from './components/Main';
+import Main from './pages/Main';
 
 const App = () => {
   return (
