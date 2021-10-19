f57bee96abe18ed152d76884c955387990434ed9
lucas-chave <lcchavesr150@gmail.com>
2021-10-19 19:22:00 -0300

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import ItemFooterResponsive from './ItemFooterResponsive';
 import ListFooter from './ListFooter';
